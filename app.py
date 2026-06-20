27c19f5c07f93833af0f1503fa2d1d32181a5ba3
prashantit98-ctrl <prashantit98@gmail.com>
2026-06-20 19:56:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def home():
-    return {
-        "project": "Deepfake Newsroom MVP",
-        "status": "online"
-    }
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
