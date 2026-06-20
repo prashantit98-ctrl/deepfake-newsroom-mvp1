af96ec13b5185f680d50108cc87c1bc06632e7ab
prashantit98-ctrl <prashantit98@gmail.com>
2026-06-20 19:47:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,19 @@
-from fastapi import FastAPI, UploadFile, File, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, UploadFile, File
 import os
-import uvicorn
 
 from utils.video import extract_frames
 from utils.metadata import get_metadata
+from utils.transcription import transcribe_audio
 from utils.report import generate_report
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
-
 os.makedirs("uploads", exist_ok=True)
 
 
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
-    )
+@app.get("/")
+async def home():
+    return {"status": "working"}
 
 
 @app.post("/analyze")
@@ -31,22 +24,11 @@ async def analyze_video(file: UploadFile = File(...)):
         f.write(await file.read())
 
     metadata = get_metadata(path)
-
-    # Whisper temporarily disabled
-    transcript = "Transcription disabled"
-
+    transcript = transcribe_audio(path)
     frames = extract_frames(path)
 
     return generate_report(
         metadata,
         transcript,
         frames
-    )
-
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "app:app",
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000))
     )
