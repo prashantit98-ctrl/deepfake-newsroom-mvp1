ff46d6aadfd806cc171601d29ca68942eb6d6fa0
prashantit98-ctrl <prashantit98@gmail.com>
2026-06-20 18:42:56 +0530

    Update transcription.py

--- utils/transcription.py
+++ utils/transcription.py
@@ -1,7 +1,38 @@
-import whisper, subprocess, os
-model=whisper.load_model('tiny')
+import whisper
+import subprocess
+import os
+
+model = None
+
+
+def get_model():
+    global model
+
+    if model is None:
+        model = whisper.load_model("tiny")
+
+    return model
+
+
 def transcribe_audio(video_path):
-    os.makedirs('outputs', exist_ok=True)
-    audio='outputs/audio.wav'
-    subprocess.run(['ffmpeg','-i',video_path,audio,'-y'],capture_output=True)
-    return model.transcribe(audio)['text']
+
+    os.makedirs("outputs", exist_ok=True)
+
+    audio = "outputs/audio.wav"
+
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-i",
+            video_path,
+            audio,
+            "-y"
+        ],
+        capture_output=True
+    )
+
+    model = get_model()
+
+    result = model.transcribe(audio)
+
+    return result["text"]
