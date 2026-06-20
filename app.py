4889f99a0bcb5cb0406c4ebaf0f3fdc382456856
prashantit98-ctrl <prashantit98@gmail.com>
2026-06-20 20:10:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import os
+
+from utils.metadata import get_metadata
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+
+os.makedirs("uploads", exist_ok=True)
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -17,6 +22,15 @@ async def home(request: Request):
 
 @app.post("/analyze")
 async def analyze(file: UploadFile = File(...)):
+
+    path = f"uploads/{file.filename}"
+
+    with open(path, "wb") as f:
+        f.write(await file.read())
+
+    metadata = get_metadata(path)
+
     return {
-        "filename": file.filename
+        "filename": file.filename,
+        "metadata": metadata
     }
